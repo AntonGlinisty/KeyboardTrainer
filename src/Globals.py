9fa87959f82ea91dfad85ae199694a38f566990b
AntonGlinisty <glinistyi.ad@phystech.edu>
2022-05-05 01:40:58 +0300

    some changes

--- src/Globals.py
+++ src/Globals.py
@@ -1,18 +1,25 @@
 stats1_txt_size = 20
-stats_area_ind_c = txt_area_ind_c = surf_area_ind_c = 3
+def_l_size = lnumb_ind_hr = ctrl_mult = bdl_sl_mult = l_nm_indf = mult_LW = l_fact = slsh_dev = 1
+let_nmb_cf = indmult_f = l_nm_indt = lnumb_ind_MD = 2
+stats_lines_count = st_time_koef = l_s_dev = ver_l_countLW = mult_DN = strt_x_d1 = dct_ind = 2
+stats_area_ind_c = txt_area_ind_c = surf_area_ind_c = strt_y_m1 = l_nm_indfi = mult_MD = 3
+up_line_size = down_line_size = 3
+left_line_size = hor_l_count = st_mist_ind = mult_UP = 4
+alt_line_mult = ctrl_line_dev = mult_SK = khc = indent1 = txt_ind_koef = strt_y_d1 = set_ev_d = 5
 stats_area_ind_s = txt_area_ind_s = surf_area_ind_s = 6
-stats_lines_count = 2
-seconds_in_min = 60
+alt_line_dev = strt_x_d2 = 9
+st_ltry_txt_mX = maxbrht = ctrl_line_mult = th_line_cou = 10
+sec_line_cou = ver_l_countDN = strt_y_m2 = 11
+fst_line_cou = ver_l_countMD = 12
+fth_line_cou = ver_l_countUP = 13
+st_ltry_txt_kY = indent3 = kwc = l4_x_ind = wds_ind1 = 15
+st_ltry_txt_kX = indent2 = ex_text_size = wds_ind2 = sft_dev = 18
+seconds_in_min = surf_info_cordy = surf_input_cordy = 60
 micros_in_seconds = 1000000
-st_mist_ind = 4
 st_mist_koef = 7
-st_time_koef = 2
 st_ltry_kX = 30
 st_ltry_mX = 21.5
 st_ltry_kY = 17
-st_ltry_txt_kX = 18
-st_ltry_txt_mX = 10
-st_ltry_txt_kY = 15
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (125, 125, 125)
@@ -21,89 +28,40 @@ GREEN = (0, 230, 0)
 YELLOW = (225, 225, 0)
 PINK = (230, 50, 230)
 RED = (255, 0, 0)
-indent1 = 5
-indent2 = 18
-indent3 = 15
 marker1 = marker2 = marker3 = marker4 = marker5 = marker6 = marker7 = True
 running = True
 filewithtext = 'text1'
 condition = 'correct'
-timelist = []
-listoftw = []
+timelist = listoftw = []
 hot_dict = {}
-l_fact = 1
-l_s_dev = 2
-txt_ind_koef = 5
 key_mod_d1 = 4096
 key_mod_d2 = 12289
 key_mod_u1 = 4097
 key_mod_u2 = 12288
 un_chec = 1000
 dict_of_s_s = 25
-fst_line_cou = 12
-sec_line_cou = 11
-th_line_cou = 10
-fth_line_cou = 13
-kwc = 15
-khc = 5
 color = {'color0': (0, 200, 0), 'color1': (20, 160, 0), 'color2': (40, 120, 0), 'color3': (80, 80, 0),
          'color4': (100, 40, 0), 'color5': (125, 0, 0), 'color6': (150, 0, 0), 'color7': (175, 0, 0),
          'color8': (200, 0, 0), 'color9': (225, 0, 0), 'color10': (255, 0, 0)}
-FPS = 60
-left_line_size = 4
+FPS = cps_dev = 60
 right_line_size = 7
-up_line_size = down_line_size = 3
 string_scroller = 85
-ex_text_size = 18
-hor_l_count = 4
-ver_l_countUP = 13
-ver_l_countMD = 12
-ver_l_countDN = 11
-ver_l_countLW = 2
-alt_line_dev = 9
-alt_line_mult = 5
-ctrl_line_dev = 5
-ctrl_line_mult = 10
-def_l_size = 1
-lnumb_ind_DN = 2.5
-lnumb_ind_MD = 2
-lnumb_ind_UP = 1.5
-lnumb_ind_hr = 1
-mult_DN = 2
-mult_MD = 3
-mult_UP = 4
-mult_LW = 1
-mult_SK = 5
-let_nmb_cf = 2
-l_nm_indf = 1
-l_nm_inds = 1.5
-l_nm_indt = 2
-l_nm_indft = 2.5
-l_nm_indfi = 3
+lnumb_ind_DN = l_nm_indft = 2.5
+lnumb_ind_UP = l_nm_inds = 1.5
 l_nm_indsi = 3.5
-indmult_f = 2
-wds_ind1 = 15
-wds_ind2 = 18
 wds_dev1 = 30
 ctrl_dev = 25
-sft_dev = 18
-cps_dev = 60
-tab_dev = 100
-slsh_dev = 1
+tab_dev = mon_size = l3_x_ind_dev = l4_x_ind_dev = 100
 tab_mult = 3.5
 cps_mult = sft_mult = 1.1
-ctrl_mult = 1
 alt_mult = 7.9
 altg_mult = 21.3
 rctrl_mult = 28.3
 rsft_mult = 13.4
 ent_mult = bck_mult = 13.6
 str_mult = 13.5
-bdl_sl_mult = 1
 spc_mult = 6.9
 ind4dev = 45
-maxbrht = 10
-dct_ind = 2
 mon_x_dev = 21 / 17
 kong_x_dev = 21 / 2
 chootxt_x = 21 / 17.1
@@ -116,14 +74,10 @@ choores_y = 70 / 8
 kong1_y_dev = 70 / 13
 kong4_y_dev = 70 / 29.5
 kong3_y_dev = 70 / 46
-set_ev_d = 5
 set_ev_u = 55
 kong_size_dev = 14 / 2.5
 mon_size_dev = 14 / 3
 l3_x_ind = 17
-l4_x_ind = 15
-l3_x_ind_dev = l4_x_ind_dev = 100
-mon_size = 100
 sf_spawn = 0
 mon_x_dev1 = 21 / 17
 mon1_y_dev1 = 14 / 3
@@ -137,27 +91,29 @@ kong3_y_devs = 70 / 46
 kong_x_devs = 14 / 2.5
 res1_x = 900
 res1_y = 600
-res2_x = 1035
-res2_y = 700
+res2_x = surface_cordx = surface_xcord = 1035
+res2_y = surface_cordy = surface_ycord = 700
 res3_x = 1125
 res3_y = 750
 scrol1 = 70
 scrol2 = 85
 scrol3 = 90
-strt_x_d1 = 2
-strt_x_d2 = 9
 strt_x_d3 = 25
-strt_y_m2 = 11
-strt_y_d1 = 5
-strt_y_m1 = 3
 l2_size = 40
 l1_dev = 75
 l1_ind = 12
-surface_cordx = 1035
-surface_cordy = 700
-surface_xcord = 1035
-surface_ycord = 700
 surf_ycord = 300
-surf_info_cordy = 60
-surf_input_cordy = 60
 counter1 = counter2 = mistakes = condition_counter = lines_counter = letter_counter = word_counter = width = 0
+space_numb = 105
+db_backsl_numb = 104
+str_line_numb = 103
+back_numb = 102
+enter_numb = 101
+r_shft_numb = 100
+r_ctrl_numb = 99
+altgr_numb = 98
+alt_numb = 97
+l_ctrl_numb = 96
+l_shft_numb = 95
+caps_numb = 94
+tab_numb = 93
